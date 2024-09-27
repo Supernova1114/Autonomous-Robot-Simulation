ad948282cae40a453e3859e6924530a4dfa83d49
Cameron Rosenthal <supernovawarriors128@gmail.com>
2024-09-27 09:15:59 -0700

    Testing new pathplanning method.

--- Mapping Solution/Assets/Scripts/Mapper.cs
+++ Mapping Solution/Assets/Scripts/Mapper.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Timeline;
 using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor.Search;
 
 public class Mapper : MonoBehaviour
 {
@@ -21,7 +23,8 @@ public class Mapper : MonoBehaviour
 
     private Vector2 mapTargetPosCeil;
 
-    public short[,] map2D;
+    public short[,] grid;
+    public List<List<short>> map2D;
     private float[,] rangeList; // Row 0: angles, Row 1: ranges.
 
     private Vector2 mapCenter; // Center for n x n map
@@ -32,35 +35,53 @@ public class Mapper : MonoBehaviour
 
     PathFinderOptions pathfinderOptions = new PathFinderOptions
     {
-        PunishChangeDirection = true,
-        UseDiagonals = false,
+        PunishChangeDirection = false,
+        UseDiagonals = true,
     };
+
+    private Vector2 previousPositionXZ;
+
+    List<short> mapRowTemp;
+
+    Vector2[] points;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        //previousPositionXZ = new Vector2(robotTransform.position.x, robotTransform.position.z);
+
         maxRaySizeCeil = Mathf.CeilToInt(lidarModule.GetMaxRayDistance());
 
         mapCenter = mapResolutionFactor * maxRaySizeCeil * Vector2.one;
 
         int mapSize = (maxRaySizeCeil * mapResolutionFactor * 2) + 1; // +1 to add a center
 
-        map2D = new short[mapSize, mapSize];
+        grid = new short[mapSize, mapSize];
+        map2D = new List<List<short>>();
+
 
         // Assign all map spaces to open
-        for (int i = 0; i < map2D.GetLength(0); i++)
+        for (int i = 0; i < mapSize; i++)
         {
-            for (int j = 0; j < map2D.GetLength(1); j++)
+            map2D.Add(new List<short>());
+
+            for (int j = 0; j < mapSize; j++)
             {
-                map2D[i, j] = 1; // Open cell
+                map2D[i].Add(1); // Open cell
             }
         }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Vector2 currentPositionXZ = new Vector2(robotTransform.position.x, robotTransform.position.z);
+
+        //ShiftMap(currentPositionXZ);
+
         rangeList = lidarModule.GetRanges();
 
         for (int i = 0; i < rangeList.GetLength(1); i++)
@@ -74,89 +95,230 @@ public class Mapper : MonoBehaviour
 
                 range = maxRaySizeCeil * mapResolutionFactor;
 
-                float slope = Mathf.Tan(angle);
-                //print(slope); 
-
-                //float slope = (Mathf.Sin(angle) * range) / (Mathf.Cos(angle) * range);
-
-
-                float linePercent = 0;
-
-                while (linePercent < 1)
-                {
-                    int sideSwitch;
-                    int bruh;
-
-                    if (angle >= 0 && angle < Mathf.PI / 2)
-                    {
-                        sideSwitch = -1;
-                        bruh = 1;
-                    }
-                    else if (angle >= Mathf.PI / 2 && angle < Mathf.PI)
-                    {
-                        sideSwitch = 1;
-                        bruh = 1;
-                    }
-                    else if (angle >= Mathf.PI && angle < Mathf.PI * 3 / 2)
-                    {
-                        sideSwitch = 1;
-                        bruh = -1;
-                    }
-                    else
-                    {
-                        sideSwitch = -1;
-                        bruh = -1;
-                    }
-
-                    // Create parametric equation of line
-                    int x = Mathf.CeilToInt(linePercent * range * -sideSwitch * 1) + (int)mapCenter.x - 1;
-                    int y = Mathf.CeilToInt(linePercent * Mathf.Abs(slope) * range * bruh * 1) + (int)mapCenter.y - 1;
-
-
-
-                    for (int r = y - 2; r <= y + 2; r++)
-                    {
-                        for (int c = x - 2; c <= x + 2; c++)
-                        {
-                            if (r < map2D.GetLength(0) && r >= 0 && c < map2D.GetLength(1) && c >= 0)
-                            {
-                                map2D[r, c] = 1;
-                            }
-                        }
-                    }
-
-                    linePercent += 0.05f;
-                }
-
-
-
-
+                //RayTraceClearCells(angle, range);
             }
             else
             {
                 // Add blocked cell (and extra padding)
-
                 range *= mapResolutionFactor;
+
+                //RayTraceClearCells(angle, maxRaySizeCeil * mapResolutionFactor);
 
                 int y = Mathf.CeilToInt(Mathf.Sin(angle) * range) + (int)mapCenter.y - 1;
                 int x = Mathf.CeilToInt(Mathf.Cos(angle) * range) + (int)mapCenter.x - 1;
 
-                for (int r = y - 2; r <= y + 2; r++)
+                for (int r = y - 5; r <= y + 5; r++)
                 {
-                    for (int c = x - 2; c <= x + 2; c++)
+                    for (int c = x - 5; c <= x + 5; c++)
                     {
-                        if (r < map2D.GetLength(0) && r >= 0 && c < map2D.GetLength(1) && c >= 0)
+                        if (r < map2D.Count && r >= 0 && c < map2D[0].Count && c >= 0)
                         {
-                            map2D[r, c] = 0;
+                            map2D[r][c] = 0; // Add Blocked Cell
                         }
                     }
                 }
             }
 
-            
+
         }
 
-        return;
+
+        // Check if there are more rays to one side
+
+        
+
+
+        //previousPositionXZ = currentPositionXZ; // TODO - Put this at end of update loop.
+
+
+
+        // Debug map print output
+        /*string output = "";
+
+        for (int i = 0; i < map2D.GetLength(0); i++)
+        {
+            for (int j = 0; j < map2D.GetLength(1); j++)
+            {
+                output += map2D[i, j] + " ";
+            }
+            output += "\n";
+        }*/
+
+        //print(output);
+        //Debug.Log(output);
+        // Clear map
+        //PlanPath1();
+        PathPlan2();
+
+        if (Time.time - lastMapClearTime > clearMapInterval)
+        {
+
+            for (int i = 0; i < map2D.Count; i++)
+            {
+                for (int j = 0; j < map2D[0].Count; j++)
+                {
+                    map2D[i][j] = 1; // Open Cell
+                }
+            }
+
+            lastMapClearTime = Time.time;
+        }
+    }
+
+    public Vector2 GetMapCenter()
+    {
+        return mapCenter;
+    }
+
+    public int GetMapResolutionFactor()
+    {
+        return mapResolutionFactor;
+    }
+
+    public Vector2 GetMapTargetPos()
+    {
+        return mapTargetPosCeil;
+    }
+
+    private void PathPlan2()
+    {
+        Vector2 currentPos = new Vector2(robotTransform.position.x, robotTransform.position.z);
+        Vector2 targetPos = new Vector2(targetPosition.position.x, targetPosition.position.z);
+
+        Vector2 mapTargetPos = ((targetPos - currentPos) * mapResolutionFactor) + new Vector2(mapCenter.x - 1, mapCenter.y - 1);
+        mapTargetPos = new Vector2(Mathf.CeilToInt(mapTargetPos.x), Mathf.CeilToInt(mapTargetPos.y));
+
+        //Note: Up is forward on map.
+
+        // Draw circle a bit larger than robot. Traverse the graph using that circle.
+
+        int currX = Mathf.CeilToInt(mapCenter.x);
+        int currY = Mathf.CeilToInt(mapCenter.y);
+        int robotRadiusMap = Mathf.CeilToInt(1.12f * mapResolutionFactor);
+        
+        points = new Vector2[200];
+
+        for (int step = 0; step < points.Length; step++)
+        {
+            /*if (CircleCollisionCheck(currX, currY - 1, robotRadiusMap) == false)
+            {
+                currY--;
+                points[step] = new Vector2(currX, currY);
+                PrintPath(currX, currY, 1);
+            }
+            else*/
+
+            float rightMag = 0;
+            float leftMag = 0;
+
+            int rightCount = 0;
+            int leftCount = 0;
+
+            for (int i = 0; i < rangeList.GetLength(1); i++)
+            {
+                float range = rangeList[1, i];
+                float angle = (rangeList[0, i] + 270) % 360;
+
+                if (range != Mathf.Infinity)
+                {
+                    if (angle >= 270 && angle < 360)
+                    {
+                        rightMag += range;
+                        rightCount++;
+                    }
+                    else if (angle >= 180 && angle < 270)
+                    {
+                        leftMag += range;
+                        leftCount++;
+                    }
+                }
+            }
+
+            float leftAvg;
+            float rightAvg;
+
+            leftAvg = leftCount == 0 ? 0 : leftMag / leftCount;
+            rightAvg = rightCount == 0 ? 0 : rightMag / rightCount;
+
+            int dir = leftAvg < rightAvg ? 1 : -1;
+
+            print("L: " + leftAvg + " | R: " + rightAvg);
+
+            if (CircleCollisionCheck(currX + dir, currY, robotRadiusMap) == false)
+            {
+                currX += dir;
+                points[step] = new Vector2(currX, currY);
+                PrintPath(currX, currY, 1);
+            }
+            else
+            {
+                currX -= dir;
+                currY += 5;
+            }
+        }
+
+        if (mapTargetPos.magnitude > 3)
+        {
+            if (points.Length >= 11)
+            {
+                targetVelocity = (points[10] - points[0]).normalized;
+                robotTransform.position += new Vector3(targetVelocity.y, 0, targetVelocity.x) * robotSpeed * Time.deltaTime;
+                targetRotation = Quaternion.LookRotation(new Vector3(targetVelocity.x, 0, -targetVelocity.y), robotTransform.up);
+            }
+
+            robotTransform.rotation = Quaternion.Slerp(robotTransform.rotation, targetRotation, 0.05f);
+        }
+
+    }
+
+    private void PrintPath(int x, int y, int radius)
+    {
+        for (int r = y - radius; r <= y + radius; r++)
+        {
+            for (int c = x - radius; c <= x + radius; c++)
+            {
+                if (r < map2D.Count && r >= 0 && c < map2D[0].Count && c >= 0)
+                {
+                    map2D[r][c] = 2; // Add path cell for debug visual
+                }
+            }
+        }
+    }
+
+    private bool CircleCollisionCheck(int mapX, int mapY, int mapRadius)
+    {
+        for (int angle = 0; angle < 360; angle++)
+        {
+            int x = Mathf.CeilToInt(mapRadius * Mathf.Cos(Mathf.Deg2Rad * angle)) + mapX;
+            int y = Mathf.CeilToInt(mapRadius * Mathf.Sin(Mathf.Deg2Rad * angle)) + mapY;
+
+            if (IsWithinMapBounds(x, y))
+            {
+                if (map2D[y][x] == 0) // blocked cell
+                {
+                    return true;
+                }
+                else
+                {
+                    //map2D[y][x] = 2; // Debug to show collision checks
+                }
+            }
+
+
+        }
+
+        return false;
+    }
+
+    private bool IsWithinMapBounds(int mapX, int mapY)
+    {
+        return mapX >= 0 && mapX < map2D[0].Count && mapY >= 0 && mapY < map2D.Count;
+    }
+
+
+    private void PlanPath1()
+    {
+        // Path plan using map
 
         Vector2 currentPos = new Vector2(robotTransform.position.x, robotTransform.position.z);
         Vector2 targetPos = new Vector2(targetPosition.position.x, targetPosition.position.z);
@@ -168,7 +330,16 @@ public class Mapper : MonoBehaviour
 
         //print("Current Pos: " + mapCenter + ", Target Pos:" + mapTargetPosCeil);
 
-        var worldGrid = new WorldGrid(map2D);
+        // Convert List<List<short>> to short[,] 2D array.
+        for (int i = 0; i < map2D.Count; i++)
+        {
+            for (int j = 0; j < map2D[0].Count; j++)
+            {
+                grid[i, j] = map2D[i][j];
+            }
+        }
+
+        var worldGrid = new WorldGrid(grid);
         var pathfinder = new PathFinder(worldGrid, pathfinderOptions);
 
         Position[] path = pathfinder.FindPath(new Position((int)mapCenter.x, (int)mapCenter.y), new Position((int)mapTargetPosCeil.x, (int)mapTargetPosCeil.y));
@@ -193,59 +364,160 @@ public class Mapper : MonoBehaviour
             {
                 targetVelocity = (worldPath[1] - worldPath[0]).normalized;
                 // Temp
+                //robotTransform.position += new Vector3(targetVelocity.x, 0, targetVelocity.y) * robotSpeed * Time.deltaTime;
                 robotTransform.position += new Vector3(targetVelocity.x, 0, targetVelocity.y) * robotSpeed * Time.deltaTime;
-
                 targetRotation = Quaternion.LookRotation(new Vector3(targetVelocity.x, 0, targetVelocity.y), robotBaseTransform.up);
             }
 
             robotBaseTransform.rotation = Quaternion.Slerp(robotBaseTransform.rotation, targetRotation, 0.05f);
+
+
+        }
+    }
+
+    private void ShiftMap(Vector2 currentPositionXZ)
+    {
+        // Shift map when robot moves.
+
+        // Find position difference vector
+
+        Vector2 positionDeltaXZ = currentPositionXZ - previousPositionXZ;
+
+        // Convert delta to map-space
+
+        Vector2 posDeltaXZMap = positionDeltaXZ * mapResolutionFactor;
+        posDeltaXZMap = new Vector2(Mathf.CeilToInt(posDeltaXZMap.y), Mathf.CeilToInt(posDeltaXZMap.x));
+
+        // Shift map according to delta.
+
+        if (posDeltaXZMap.x > 0)
+        {
+            foreach (List<short> row in map2D)
+            {
+                row.RemoveRange(0, (int)posDeltaXZMap.x);
+                for (int i = 0; i < (int)posDeltaXZMap.x; i++)
+                {
+                    row.Add(1); // Add clear cell
+                }
+            }
+        }
+        else if (posDeltaXZMap.x < 0)
+        {
+            int posDeltaX = (int)Mathf.Abs(posDeltaXZMap.x);
+
+            print(posDeltaX);
+
+            foreach (List<short> row in map2D)
+            {
+                row.RemoveRange(row.Count - posDeltaX, posDeltaX);
+                for (int i = 0; i < posDeltaX; i++)
+                {
+                    row.Insert(0, 1); // Add clear cell
+                }
+            }
         }
 
-        // Debug map print output
-        /*string output = "";
-
-        for (int i = 0; i < map2D.GetLength(0); i++)
+        /*if (posDeltaXZMap.y > 0)
         {
-            for (int j = 0; j < map2D.GetLength(1); j++)
+            // Add to top, remove from bottom
+
+            // Remove from bottom
+            map2D.RemoveRange(map2D.Count - (int)posDeltaXZMap.y, (int)posDeltaXZMap.y);
+
+            // Add to top
+            for (int i = 0; i < (int)posDeltaXZMap.y; i++)
             {
-                output += map2D[i, j] + " ";
-            }
-            output += "\n";
-        }*/
+                mapRowTemp = new List<short>(map2D.Count);
 
-        //print(output);
-        //Debug.Log(output);
-        // Clear map
-
-        /*if (Time.time - lastMapClearTime > clearMapInterval)
-        {
-            
-
-            *//*for (int i = 0; i < map2D.GetLength(0); i++)
-            {
-                for (int j = 0; j < map2D.GetLength(1); j++)
+                for (int j = 0; j < map2D.Count; j++)
                 {
-                    map2D[i, j] = 1; // Open Cell
+                    mapRowTemp.Add(1); // Open cell
                 }
-            }*//*
 
-            lastMapClearTime = Time.time;
+                map2D.Insert(0, mapRowTemp);
+            }
+        }
+        else if (posDeltaXZMap.y < 0)
+        {
+            int posDeltaY = (int)Mathf.Abs(posDeltaXZMap.y);
+
+            // Remove from top, add from bottom
+
+            // Remove from top
+            map2D.RemoveRange(0, posDeltaY);
+
+            // Add from bottom
+            for (int i = 0; i < posDeltaY; i++)
+            {
+
+                mapRowTemp = new List<short>(map2D.Count);
+
+                for (int j = 0; j < map2D.Count; j++)
+                {
+                    mapRowTemp.Add(1); // Open cell
+                }
+
+                map2D.Add(mapRowTemp);
+            }
         }*/
+
     }
 
-    public Vector2 GetMapCenter()
+    private void RayTraceClearCells(float angle, float range)
     {
-        return mapCenter;
-    }
+        float slope = Mathf.Tan(angle);
+        //print(slope); 
 
-    public int GetMapResolutionFactor()
-    {
-        return mapResolutionFactor;
-    }
+        //float slope = (Mathf.Sin(angle) * range) / (Mathf.Cos(angle) * range);
 
-    public Vector2 GetMapTargetPos()
-    {
-        return mapTargetPosCeil;
+
+        float linePercent = 0;
+
+        while (linePercent < 1)
+        {
+            int sideSwitch;
+            int bruh;
+
+            if (angle >= 0 && angle < Mathf.PI / 2)
+            {
+                sideSwitch = -1;
+                bruh = 1;
+            }
+            else if (angle >= Mathf.PI / 2 && angle < Mathf.PI)
+            {
+                sideSwitch = 1;
+                bruh = 1;
+            }
+            else if (angle >= Mathf.PI && angle < Mathf.PI * 3 / 2)
+            {
+                sideSwitch = 1;
+                bruh = -1;
+            }
+            else
+            {
+                sideSwitch = -1;
+                bruh = -1;
+            }
+
+            // Create parametric equation of line
+            int x = Mathf.CeilToInt(linePercent * range * -sideSwitch * 1) + (int)mapCenter.x - 1;
+            int y = Mathf.CeilToInt(linePercent * Mathf.Abs(slope) * range * bruh * 1) + (int)mapCenter.y - 1;
+
+
+
+            for (int r = y - 5; r <= y + 5; r++)
+            {
+                for (int c = x - 5; c <= x + 5; c++)
+                {
+                    if (r < map2D.Count && r >= 0 && c < map2D[0].Count && c >= 0)
+                    {
+                        map2D[r][c] = 1; // Clear cell
+                    }
+                }
+            }
+
+            linePercent += 0.05f;
+        }
     }
 
 }
